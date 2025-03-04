08d4bf42c7a45d635e700be8700282b7830d5823
prathyush04 <113966694+prathyush04@users.noreply.github.com>
2025-03-04 21:56:41 +0530

    Update usingLinkedList.java

--- usingLinkedList.java
+++ usingLinkedList.java
@@ -49,3 +49,17 @@ public class usingLL {
         return (top == null);
     }
 }
+public static void main(String[] args) {
+        usingLL stack = new usingLL(5);
+        
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        stack.display(); // Output: 1 2 3 4 5
+
+        stack.pop();
+        stack.display(); // Output: 1 2 3 4
+    }
